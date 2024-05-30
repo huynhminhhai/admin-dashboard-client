9fe6273134036eca50d9a1a912ee1cf9c74e780e
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-05-30 13:46:22 +0700

    [UPDATE] building a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
-import { CssBaseline, PaletteMode, ThemeProvider, createTheme } from '@mui/material'
+import { CssBaseline, PaletteMode, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
 import { useSelector } from 'react-redux';
 import { RootState } from './store';
 import { useMemo } from 'react';
 import { themeSettings } from './theme';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Dashboard, Layout } from './scenes';
 
 function App() {
+    const mode = useSelector(
+        (state: RootState) => state.global.mode
+    ) as PaletteMode;
 
-  const mode = useSelector((state: RootState) => state.global.mode) as PaletteMode
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-  return (
-    <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
-    </div>
-  );
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Routes>
+                        <Route element={<Layout />}>
+                            <Route
+                                path="/"
+                                element={<Navigate to="/dashboard" replace />}
+                            />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Route>
+                    </Routes>
+                </ThemeProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
